496125c8b95e1d3178a5fff2f701785485e61368
Semen <semion.bahtin@gmail.com>
2019-12-13 15:48:28 +0500

    widget added

--- docs/js/widget.js
+++ docs/js/widget.js
@@ -1,15 +1,16 @@
-var WeMatcherWidget = function(id) {
+var WeMatcherWidget = function (id) {
     var div = document.getElementById(id);
-    if(!div){
+    if (!div) {
         return;
     }
 
-    $('#'+id).html('<object id="wematcher_frame" data="https://wematcher.com/WeMatcher/public" style="width:100%;"></object>');
+    $('#' + id).html('<iframe id="wematcher_frame" allow="camera; microphone" src="https://applet.wematcher.com" frameborder="0" width="1200" height="560" style="border-radius: 4px;"></iframe>');
 
-    window.onmessage = function(e) {
-        if(e.data.type && e.data.type == 'height') {
-            $('#wematcher_frame').height(e.data.value);
-        }
-    }
+    // window.onmessage = function(e) {
+    //     if(e.data.type && e.data.type == 'size') {
+    //         $('#wematcher_frame').height(e.data.value.height);
+    //         $('#wematcher_frame').width(e.data.value.width);
+    //     }
+    // }
 }
 
